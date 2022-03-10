1a1876f6037d137397c5399898a275ea434cfc58
chrilona <christabelalona@gmail.com>
2022-03-10 08:36:55 +0300

    first assesesment

---
+++ src/kotlin1.kt
@@ -0,0 +1,27 @@
+fun main(){
+    name()
+    text()
+    println(two(30,6))
+}
+fun one(){
+    var statement = "CodeHive"
+    var x =statement.get(4)
+
+}
+
+fun two( x:Int, y:Int): Int {
+    var div = 30/6
+    return div
+
+}
+fun  text() {
+     val name = "lona"
+     var age = 42
+    println("My name is $name and iam $age years old")
+
+}
+
+fun name(){
+    var title ="When is today?"
+    println(title.length)
+}
